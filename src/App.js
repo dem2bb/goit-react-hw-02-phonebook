62ffedeb6d3e797cd8742f9ee84e0989df80a1ee
Maxim Kazachuk <dem2bb1@gmail.com>
2021-02-25 16:37:49 +0200

    added LocalStorage save

--- src/App.js
+++ src/App.js
@@ -46,6 +46,18 @@ class App extends Component {
     });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) this.setState({ contacts: parsedContacts });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   render() {
     return (
       <div className="container">
